44d04e06a6e379460f4a005813da582e1a287b11
yonekura-k <k.yonekura716@gmail.com>
2023-04-10 14:18:45 +0900

    Docker実装

--- README.md
+++ README.md
@@ -3,7 +3,15 @@
 論文翻訳補助ツールです。
 
 実行方法：
+【通常】
+
 python main.py
+
+【Docker】
+
+docker build -t paper_format .
+
+docker run -v $(pwd):/app -it paper_format
 
 詳細は以下の記事を参照ください。
 https://yonesuke0716.hatenablog.com/entry/2023/01/23/100836
--- translate.py
+++ translate.py
@@ -1,24 +1,29 @@
+import os
+from tqdm import tqdm
 from googletrans import Translator
 
 
 def english_to_japanese(text_file_list, format_list):
     f_num = 0
+    os.makedirs("output", exist_ok=True)
     print("修正したテキストを翻訳中・・・")
     translator = Translator()
     # str_thresh文字以上超えた出力
     if len(text_file_list) > 0:
-        for t_file in text_file_list:
+        for text_file in text_file_list:
             f_num += 1
-            with open("output_{}.txt".format(f_num), mode="w", encoding="utf-8") as f:
-                for t_1 in t_file:
-                    translated = translator.translate(t_1, dest="ja")
-                    print(t_1, file=f)
+            with open(
+                "output/output_{}.txt".format(f_num), mode="w", encoding="utf-8"
+            ) as f:
+                for t_num in tqdm(range(len(text_file))):
+                    translated = translator.translate(text_file[t_num], dest="ja")
+                    print(text_file[t_num], file=f)
                     print(translated.text, file=f)  # 翻訳後の文章
 
     # str_thresh文字以内の出力
     f_num += 1
-    with open("output_{}.txt".format(f_num), mode="w", encoding="utf-8") as f:
-        for t_2 in format_list:
-            translated = translator.translate(t_2, dest="ja")
-            print(t_2, file=f)
+    with open("output/output_{}.txt".format(f_num), mode="w", encoding="utf-8") as f:
+        for f_num in tqdm(range(len(format_list))):
+            translated = translator.translate(format_list[f_num], dest="ja")
+            print(format_list[f_num], file=f)
             print(translated.text, file=f)  # 翻訳後の文章
